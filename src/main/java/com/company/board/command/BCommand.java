713d5e29e1fe736d3eba531a4fbc3d809a7a1954
ici <ici@DESKTOP-J6RS3LU>
2023-04-20 10:10:30 +0900

    asd

---
+++ src/main/java/com/company/board/command/BCommand.java
@@ -0,0 +1,9 @@
+package com.company.board.command;
+
+import org.springframework.ui.Model;
+
+public interface BCommand {
+
+	public void execute(Model model);
+	
+}
---
+++ src/main/java/com/company/board/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.company.board.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+	
+	@RequestMapping(value = "/write_form")
+	public String write_form() {
+		
+		return "writeForm";
+	}
+	@RequestMapping(value = "/write")
+	public String write(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("request", request);
+		
+		return "";
+	}
+
+}
